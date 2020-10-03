e296961d5c95a4ef767ac6d610a0525ab68d27ae
guruc-134 <70446767+guruc-134@users.noreply.github.com>
2020-10-03 14:32:57 +0530

    added a very efficient recursive Fibonacci program
    a Fibonacci recursive algorithm which is very fast than the traditional recursive solution

---
+++ EffecientRecursuionFibonacci.py
@@ -0,0 +1,8 @@
+def fib(n):
+    if n<=1:
+        return (n,0)
+    else:
+        (a,b)=fib(n-1)
+        return (a+b,a)
+number=int(input("enter a number to find the nth fibonacci number :"))
+print(fib(number)[0])
