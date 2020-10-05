9642e8fc6044f2f85bc146973083e6d01facc3f9
Karthik08-github <60476477+Karthik08-github@users.noreply.github.com>
2020-10-05 15:36:53 +0530

    PLZ CHECK THE CHANGES WHICH I HAD DONE
    AND REVIEW THE CODE

--- Breadth first search/Binary Tree Level Order Traversal.cpp
+++ Breadth first search/Binary Tree Level Order Traversal.cpp
@@ -1,12 +1,5 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+/*
+ * HAVE TOKEN THE LOGIC IMPLENTATION FROM C++ PRIMER BOOK
  * };
  */
 class Solution {
